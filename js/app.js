ef0ebef65b5624c0ff47d89e2842d94edbffcabf
huangxd <649924757@qq.com>
2022-10-05 14:41:40 +0800

    五线谱将easyscore调整为vexflow

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 Object.defineProperty(Vue.prototype, 'WebMidi', { value: WebMidi });
-const { Factory } = Vex.Flow;
+const { Renderer, Stave, StaveNote, Voice, Formatter, Accidental, StaveConnector } = Vex.Flow;
 
 
 var app = new Vue({
@@ -19,22 +19,22 @@ var app = new Vue({
         pageWidth: window.innerWidth,
         keyWidth: null,
         blackKeyWidth: null,
-        vf: null,
-        score: null,
-        system: null,
-        lowNotes: null,
-        upNotes: null,
-        midiCode: ['A0', 'A#0', 'B0',
-            'C1', 'C#1', 'D1', 'D#1', 'E1', 'F1', 'F#1', 'G1', 'G#1', 'A1', 'A#1', 'B1',
-            'C2', 'C#2', 'D2', 'D#2', 'E2', 'F2', 'F#2', 'G2', 'G#2', 'A2', 'A#2', 'B2',
-            'C3', 'C#3', 'D3', 'D#3', 'E3', 'F3', 'F#3', 'G3', 'G#3', 'A3', 'A#3', 'B3',
-            'C4', 'C#4', 'D4', 'D#4', 'E4', 'F4', 'F#4', 'G4', 'G#4', 'A4', 'A#4', 'B4',
-            'C5', 'C#5', 'D5', 'D#5', 'E5', 'F5', 'F#5', 'G5', 'G#5', 'A5', 'A#5', 'B5',
-            'C6', 'C#6', 'D6', 'D#6', 'E6', 'F6', 'F#6', 'G6', 'G#6', 'A6', 'A#6', 'B6',
-            'C7', 'C#7', 'D7', 'D#7', 'E7', 'F7', 'F#7', 'G7', 'G#7', 'A7', 'A#7', 'B7',
-            'C8'],
+        midiCode: ['a/0', 'a#/0', 'b/0',
+            'c/1', 'c#/1', 'd/1', 'd#/1', 'e/1', 'f/1', 'f#/1', 'g/1', 'g#/1', 'a/1', 'a#/1', 'b/1',
+            'c/2', 'c#/2', 'd/2', 'd#/2', 'e/2', 'f/2', 'f#/2', 'g/2', 'g#/2', 'a/2', 'a#/2', 'b/2',
+            'c/3', 'c#/3', 'd/3', 'd#/3', 'e/3', 'f/3', 'f#/3', 'g/3', 'g#/3', 'a/3', 'a#/3', 'b/3',
+            'c/4', 'c#/4', 'd/4', 'd#/4', 'e/4', 'f/4', 'f#/4', 'g/4', 'g#/4', 'a/4', 'a#/4', 'b/4',
+            'c/5', 'c#/5', 'd/5', 'd#/5', 'e/5', 'f/5', 'f#/5', 'g/5', 'g#/5', 'a/5', 'a#/5', 'b/5',
+            'c/6', 'c#/6', 'd/6', 'd#/6', 'e/6', 'f/6', 'f#/6', 'g/6', 'g#/6', 'a/6', 'a#/6', 'b/6',
+            'c/7', 'c#/7', 'd/7', 'd#/7', 'e/7', 'f/7', 'f#/7', 'g/7', 'g#/7', 'a/7', 'a#/7', 'b/7',
+            'c/8'],
         lowNoteMidiArr: [],
         upNoteMidiArr: [],
+        scoreContext: null,
+        staveTreble: null,
+        staveBass: null,
+        upVoice: null,
+        lowVoice: null,
     },
     created: function () {
 
@@ -53,8 +53,10 @@ var app = new Vue({
             }
         });
 
-        // this.renderChord('', '')
-        // this.renderChord('C2 E2 G3 Bb3', 'C4 D#4 E4 G4 Bb4');
+        this.initScore()
+        this.renderScore(['a/0', 'e/2', 'g/3', 'bb/3', 'c/4', 'd#/4', 'e/4', 'g/4', 'a/3'])
+
+        // this.renderScore(['c/3'])
     },
     watch: {
         selectedMidiInputId(newMidiInputId) {
@@ -153,42 +155,111 @@ var app = new Vue({
             };
         },
 
-        renderChord(lowNoteArr, upNoteArr) {
-            const scorePanel = document.getElementById("scorePanel")
-            while (scorePanel.hasChildNodes()) {
-                scorePanel.removeChild(scorePanel.lastChild);
+        initScore() {
+            const div = document.getElementById('scorePanel');
+            const renderer = new Renderer(div, Renderer.Backends.SVG);
+
+            renderer.resize(180, 320);
+            this.scoreContext = renderer.getContext();
+            this.scoreContext.setFont('Arial', 10);
+
+            this.staveTreble = new Stave(10, 60, 180);
+            this.staveBass = new Stave(10, 140, 180);
+
+            this.staveTreble.addClef('treble')
+            this.staveBass.addClef('bass')
+
+            const connector = new StaveConnector(this.staveTreble, this.staveBass);
+            connector.setType(StaveConnector.type.SINGLE);
+            connector.setContext(this.scoreContext);
+
+            this.staveTreble.setContext(this.scoreContext).draw();
+            this.staveBass.setContext(this.scoreContext).draw();
+            connector.draw();
+        },
+
+        renderScore(noteArr) {
+            if (this.upVoice != null) {
+                this.upVoice.tickables[0].attrs.el.remove()
+            }
+            if (this.lowVoice != null) {
+                this.lowVoice.tickables[0].attrs.el.remove()
             }
 
-            this.vf = new Factory({
-                renderer: { elementId: 'scorePanel', width: 500, height: 500 },
-            });
+            const context = this.scoreContext
+            const staveTreble = this.staveTreble
+            const staveBass = this.staveBass
 
-            this.score = this.vf.EasyScore();
-            this.system = this.vf.System();
+            const upNotes = []
+            const lowNotes = []
 
-            // TODO： 需要处理没有note的情况
-            this.lowNotes = this.score.notes(`(${lowNoteArr})/1`, {clef:'bass'})
-            this.upNotes = this.score.notes(`(${upNoteArr})/1`)
+            noteArr.forEach(function(key) {
+                if (parseInt(key.split('/')[1]) >= 4) {
+                    upNotes.push(key)
+                } else {
+                    lowNotes.push(key)
+                }
+            })
 
-            this.system
-                .addStave({
-                    voices: [
-                        this.score.voice(this.upNotes),
-                    ],
+            console.log(upNotes)
+            console.log(lowNotes)
+
+            const voices = [];
+
+            if (upNotes.length !== 0) {
+                const upStaveNote = new StaveNote({
+                    keys: upNotes,
+                    duration: "w",
+                    align_center: true,
                 })
-                .addClef('treble')
 
-            this.system
-                .addStave({
-                    voices: [
-                        this.score.voice(this.lowNotes),
-                    ],
+                for (let i = 0; i < upNotes.length; ++i) {
+                    if (upNotes[i].split('/')[0].length > 1) {
+                        upStaveNote.addModifier(new Accidental(upNotes[i].split('/')[0][1]), i)
+                    }
+                }
+
+                const notesUp = [
+                    upStaveNote,
+                ];
+
+                this.upVoice = new Voice({}).addTickables(notesUp)
+
+                voices.push(this.upVoice)
+            }
+
+            if (lowNotes.length !== 0) {
+                const lowStaveNote = new StaveNote({
+                    keys: lowNotes,
+                    duration: "w",
+                    align_center: true,
+                    clef: 'bass',
                 })
-                .addClef('bass')
 
-            this.system.addConnector()
+                for (let i = 0; i < lowNotes.length; ++i) {
+                    if (lowNotes[i].split('/')[0].length > 1) {
+                        lowStaveNote.addModifier(new Accidental(lowNotes[i].split('/')[0][1]), i)
+                    }
+                }
 
-            this.vf.draw();
+                const notesLow = [
+                    lowStaveNote,
+                ];
+
+                this.lowVoice = new Voice({}).addTickables(notesLow)
+
+                voices.push(this.lowVoice)
+            }
+
+            new Formatter().joinVoices(voices).format(voices, 120);
+
+            if (upNotes.length !== 0) {
+                this.upVoice.draw(context, staveTreble);
+            }
+            if (lowNotes.length !== 0) {
+                this.lowVoice.draw(context, staveBass);
+            }
+
         },
     }
 });
